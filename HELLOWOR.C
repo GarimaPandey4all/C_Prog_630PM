4aac6a78a28a7aeb762a38ae3b71ab988133577f
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2020-04-23 23:01:50 -0700

    Add files via upload

---
+++ HELLOWOR.C
@@ -0,0 +1,22 @@
+#include<stdio.h> //Standard Input/Ouput
+#include<conio.h> //Console Input/Output
+//#--> Preprocessor
+//#include--> Preprocessor Directive
+//Input-->scanf() -->Read input from a user
+//Output-->printf() -->Output show on the screen/Console
+
+/*
+	Name: Amanpreet
+	Code: Hello World Program
+	Developer: Garima Pandey
+*/
+
+void main() //Entry point of a program
+{
+       clrscr(); //Clear screen used to clear the previous output
+
+	printf("Hello World.\n");
+
+      getch(); //Get Character
+
+}
